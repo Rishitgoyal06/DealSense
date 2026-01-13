b01ef01def3d623c4d68ef1124169cfb44571b9d
rishitgoyal06 <rishitgoyal6@gmail.com>
2026-01-13 10:27:02 +0530

    feat: connect to the mongoDB atlas

---
+++ backend/src/db/index.js
@@ -0,0 +1,14 @@
+import mmongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mmongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+};
+
+export  default connectDB;
