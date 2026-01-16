2aaf06b7b9050f8a8197bbd7664add2fed2c7279
rishitgoyal06 <rishitgoyal6@gmail.com>
2026-01-16 14:41:13 +0530

    fix: fixed the mongoose spelling error which was causing the import error

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,9 +1,9 @@
-import mmongoose from 'mongoose';
+import mongoose from 'mongoose';
 import { DB_NAME } from '../constants.js';
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mmongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
