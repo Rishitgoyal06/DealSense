336bb5952eda8824297896dec645f796bfcd5965
rishitgoyal06 <rishitgoyal6@gmail.com>
2026-01-21 10:33:21 +0530

    fix: fixed the cors issue which was showing error when connecting backend with the frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ import followUpRoutes from './routes/followUpRoutes.js';
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: true,
     credentials: true
 }));
 
